d010375099d79123ac4d19c4169224cefc74a76c
Frank Wang <yf.frank.wang@outlook.com>
2019-09-11 06:30:23 +0000

    1a6e0ed by travis-ci

--- src/main/java/panda/demo/action/test/TestAction.java
+++ src/main/java/panda/demo/action/test/TestAction.java
@@ -1,12 +1,15 @@
 package panda.demo.action.test;
 
+import java.io.IOException;
 import java.util.Map;
 
 import panda.app.action.BaseAction;
+import panda.io.MimeTypes;
 import panda.mvc.annotation.At;
 import panda.mvc.annotation.To;
 import panda.mvc.annotation.param.Param;
 import panda.mvc.view.Views;
+import panda.servlet.HttpServletResponser;
 
 
 @At("/test")
@@ -19,5 +22,16 @@ public class TestAction extends BaseAction {
 	@At
 	@To(Views.SXML)
 	public void xml(@Param Map m) {
+	}
+
+	@At
+	@To(Views.NONE)
+	public void echo(@Param("c") String cs, @Param("t") String txt) throws IOException {
+		HttpServletResponser hsrs = new HttpServletResponser(getRequest(), getResponse());
+		hsrs.setContentType(MimeTypes.TEXT_HTML);
+		hsrs.setMaxAge(0);
+		hsrs.setCharset(cs);
+		hsrs.writeHeader();
+		hsrs.writeText(txt);
 	}
 }
