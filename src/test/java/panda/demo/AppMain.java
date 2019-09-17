f75ed1fddb06b13d32f0ee3a1fd483277b545aae
Frank Wang <yf.frank.wang@outlook.com>
2019-09-17 03:43:35 +0000

    85d09c3 by travis-ci

--- src/test/java/panda/demo/AppMain.java
+++ src/test/java/panda/demo/AppMain.java
@@ -1,8 +1,12 @@
 package panda.demo;
 
+import panda.io.Files;
+
 public class AppMain {
 	public static void main(String[] args) {
 		try {
+			Files.makeDirs("web/WEB-INF/_sqlite");
+
 			System.setProperty("tomcat.util.http.parser.HttpParser.requestTargetAllow", "|{}&");
 			AppLaunch.main(new String[] { 
 					"--port", "80",
